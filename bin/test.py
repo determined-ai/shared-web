84e4089853fbef4abd9a51348b08c8c3cd252d05
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-05-26 12:01:49 -0700

    reduce duplication in test script

--- bin/test.py
+++ bin/test.py
@@ -6,6 +6,11 @@ SAAS_REPO='https://github.com/determined-ai/saas'
 CORE_REPO='https://github.com/determined-ai/determined'
 SM_DIR = 'src/shared'
 
+users = [
+    {'name': 'saas', 'repo': SAAS_REPO, 'web_dir': 'web'},
+    {'name': 'core', 'repo': CORE_REPO, 'web_dir': 'webui/react'},
+]
+
 
 # Python program to print
 # colored text and background
@@ -40,24 +45,19 @@ def get_current_hash():
     return get_output('git rev-parse HEAD')
 
 
-def build_saas(sm_hash: str):
-    clone_dir = '/tmp' / pathlib.Path('saas')
-    web_dir = 'web'
-    run(f'rm -rf {clone_dir}; git clone {SAAS_REPO} {clone_dir} --recurse-submodules')
+def setup_user(user, sm_hash: str):
+    clone_dir = '/tmp' / pathlib.Path(user['name'])
+    web_dir = user['web_dir']
+    run(f'rm -rf {clone_dir}; git clone {user["repo"]} {clone_dir} --recurse-submodules')
     run(f'cd {clone_dir} && git checkout master && git pull')
     run(f'cd {clone_dir}/{web_dir}/{SM_DIR} && git checkout {sm_hash}')
-    run(f'cd {clone_dir} && make -C {web_dir} node_modules/done.stamp build')
 
 
-def build_core(sm_hash: str):
-    clone_dir = '/tmp' / pathlib.Path('core')
-    web_dir = 'webui/react'
-    run(f'rm -rf {clone_dir}; git clone {CORE_REPO} {clone_dir} --recurse-submodules')
-    run(f'cd {clone_dir} && git checkout master && git pull')
-    run(f'cd {clone_dir}/{web_dir}/{SM_DIR} && git checkout {sm_hash}')
-    run(f'cd {clone_dir} && make -C {web_dir} node_modules/done.stamp build')
+def test(sm_hash: str):
+    for user in users:
+        setup_user(user, sm_hash)
+        run(f'cd /tmp/{user["name"]}/{user["web_dir"]} && make build')
 
 
 if __name__ == '__main__':
-    build_saas(get_current_hash())
-    build_core(get_current_hash())
+    test(get_current_hash())
